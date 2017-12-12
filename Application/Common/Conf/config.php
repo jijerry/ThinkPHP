3e28adfb5213730bc39f8a5653b6ec264830e0b6
jijerry <jipengbo@xunlei.com>
2017-12-12 16:50:40 +0800

    develop

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+    //'配置项'=>'配置值'
+    'URL_ROUTER_ON' =>true,
+
+    //路由重写
+    'URL_ROUTE_RULES'=>array(
+        'blogs/:id' => array('Index/read'),
+        'article/:id' => array('Article/show')
+    ),
+    'URL_MAP_RULES'=>array(
+        'new/top' => 'Index/top?type=top'
+    ),
+
+    //数据库参数
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'think',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'think_',
+
+);
