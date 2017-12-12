3c726a292a528247baf707f535b52667d409d03a
jijerry <jipengbo@xunlei.com>
2017-12-12 20:19:41 +0800

    跳转

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -16,7 +16,7 @@ class IndexController extends Controller {
         $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover,{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
 
-    public function read($id){
+    public function read($id=0){
 
         echo "read page with ".$id;
     }
@@ -26,6 +26,8 @@ class IndexController extends Controller {
         echo "$year".$month;
     }
 
+
+    //前置和后置操作
     public function _before_top(){
         echo "before top page";
     }
@@ -37,5 +39,26 @@ class IndexController extends Controller {
     public function _after_top(){
         echo "after top page";
     }
+
+
+    //跳转
+    public function user(){
+
+        $User = M('User');
+        $data['username'] = 'Think';
+        $data['email'] = 'Think@gmail.com';
+        $result =$User->add($data);
+        $result =1;
+
+        if (!$result){
+
+            $this->success('success','Home/User/addUser');
+        }else{
+            $this->error('failed','Home/Article/error',5);
+        }
+    }
+
+    //重定向
+    
 
 }
---
+++ Application/Home/Controller/UserController.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerry
+ * Date: 2017/12/12
+ * Time: 19:46
+ */
+
+namespace Home\Controller;
+
+use Think\Controller;
+
+class UserController extends Controller{
+
+    public function addUser(){
+
+        echo 'add user done';
+    }
+}
