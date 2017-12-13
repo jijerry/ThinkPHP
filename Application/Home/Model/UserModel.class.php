bb1a8251ac7f1b0f77b17492ece16e78827c5feb
jijerry <jipengbo@xunlei.com>
2017-12-13 15:32:15 +0800

    操作数据库方法testDemo

---
+++ Application/Home/Model/UserModel.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerry
+ * Date: 2017/12/13
+ * Time: 11:45
+ *
+ * 模型类操作数据库
+ * thinkPHP会自动定位到表
+ * protected $tablePrefix =''
+ * protected $tableName ='users'
+ */
+
+namespace Home\Model;
+
+use Think\Model;
+
+class UserModel extends Model{
+
+    public function testDemo(){
+
+    }
+
+}
